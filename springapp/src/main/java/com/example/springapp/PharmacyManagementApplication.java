297603c16fb53ca6cc9081d6b5ae04381aa79dd6
Iamneo <techadmin@iamneo.ai>
2023-10-05 05:49:23 +0000

    Backup Commit

--- springapp/src/main/java/com/example/springapp/PharmacyManagementApplication.java
+++ springapp/src/main/java/com/example/springapp/PharmacyManagementApplication.java
@@ -1,3 +1,10 @@
+// PharmacyManagementApplication.java
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PharmacyManagementApplication {
-    
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyManagementApplication.class, args);
+    }
 }
